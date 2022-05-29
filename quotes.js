68c8bbaffc6e0d5d856424b1889fe63774474e82
mtncloud <105944343+mtncloud@users.noreply.github.com>
2022-05-29 13:01:29 -0500

    first commit

---
+++ quotes.js
@@ -0,0 +1,42 @@
+const localQuotes = [
+  {
+    text: 'Our ability to achieve happiness and success depends on the strength of our wings.',
+    author: 'Catherine Pulsifer',
+  },
+  {
+    text: 'To go against the dominant thinking of your friends, of most of the people you see every day, is perhaps the most difficult act of heroism you can perform.',
+    author: 'Theodore H. White',
+  },
+  {
+    text: 'Gratitude makes sense of our past, brings peace for today, and creates a vision for tomorrow.',
+    author: 'Melody Beattie',
+  },
+  {
+    text: 'Into each life rain must fall but rain can be the giver of life and it is all in your attitude that makes rain produce sunshine.',
+    author: 'Byron Pulsifer',
+  },
+  {
+    text: 'We are all inclined to judge ourselves by our ideals; others, by their acts.',
+    author: 'Harold Nicolson',
+  },
+  {
+    text: 'Nothing is a waste of time if you use the experience wisely.',
+    author: 'Rodin',
+  },
+  {
+    text: 'If one way be better than another, that you may be sure is natures way.',
+    author: 'Aristotle',
+  },
+  {
+    text: 'Here is one quality that one must possess to win, and that is definiteness of purpose, the knowledge of what one wants, and a burning desire to possess it.',
+    author: 'Napoleon Hill',
+  },
+  {
+    text: 'It is not in the stars to hold our destiny but in ourselves.',
+    author: 'William Shakespeare',
+  },
+  {
+    text: 'Using the power of decision gives you the capacity to get past any excuse to change any and every part of your life in an instant.',
+    author: 'Tony Robbins',
+  },
+];
